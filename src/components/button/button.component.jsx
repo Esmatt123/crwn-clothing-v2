15e91c289110febdaacf85baa0a8845153a6ad90
Esmatt123 <esmatt98@hotmail.com>
2022-12-17 16:45:03 +0100

    created sign up component, the form input component, button component

---
+++ src/components/button/button.component.jsx
@@ -0,0 +1,25 @@
+/*default
+
+inverted
+
+google sign in
+
+*/
+import "./button.styles.scss";
+const BUTTON_TYPE_CLASSES = {
+  google: "google-sign-in",
+  inverted: "inverted",
+};
+
+const Button = ({ children, buttonType, inputOptions }) => {
+  return (
+    <button
+      className={`button-container ${BUTTON_TYPE_CLASSES[buttonType]}`}
+      {...inputOptions}
+    >
+      {children}
+    </button>
+  );
+};
+
+export default Button;
--- src/routes/sign-in/sign-in.component.jsx
+++ src/routes/sign-in/sign-in.component.jsx
@@ -1,9 +1,19 @@
+import { useEffect } from "react";
+import { getRedirectResult } from "firebase/auth";
+import SignUpForm from "../../sign-up-form/sign-up-form.component";
 import {
+  auth,
   signInWithGooglePopup,
+  signInWithGoogleRedirect,
   createUserDocumentFromAuth,
 } from "../../utils/firebase/firebase.utils";
 
 const SignIn = () => {
+  useEffect(async () => {
+    const response = await getRedirectResult(auth);
+    console.log(response);
+  }, []);
+
   const logGoogleUser = async () => {
     const { user } = await signInWithGooglePopup();
     const userDocRef = await createUserDocumentFromAuth(user);
@@ -12,6 +22,10 @@ const SignIn = () => {
     <div>
       <h1>Sign In Page</h1>
       <button onClick={logGoogleUser}>Sign in with Google Popup</button>
+      <button onClick={signInWithGoogleRedirect}>
+        Sign in with Google Redirect
+      </button>
+      <SignUpForm />
     </div>
   );
 };
